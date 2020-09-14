614d2353e796e3de2301bc14e5ec60a65c2cd508
José Manuel Herrero <barcelona20180811@gmail.com>
2020-09-14 11:55:09 +0200

    Updates

---
+++ precache-manifest.59f73e017cfea9b929a9e7d09210fa02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5046b797416aa2e8ab0f3dd84badfdc",
+    "url": "/react-example-jmh/index.html"
+  },
+  {
+    "revision": "30ea6ddd4d30c8560ba9",
+    "url": "/react-example-jmh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5567f761d182c6018d6",
+    "url": "/react-example-jmh/static/js/2.96b30471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example-jmh/static/js/2.96b30471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ea6ddd4d30c8560ba9",
+    "url": "/react-example-jmh/static/js/main.ffa5d59e.chunk.js"
+  },
+  {
+    "revision": "18d3614d9bd94e083660",
+    "url": "/react-example-jmh/static/js/runtime-main.47890b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example-jmh/static/media/logo.5d5d9eef.svg"
+  }
+]);
